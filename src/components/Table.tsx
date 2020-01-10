3294c85077bea5ade420021b8a58923a261ad673
Ricardo de Arruda <settiricardo@gmail.com>
2020-01-10 05:22:44 +0200

    display store values in table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,25 @@
-import React,{useState, useContext} from 'react'
-import {Context, ContextProps} from './Store'
+import React, { useState, useContext, useEffect, forwardRef} from 'react'
 import MaterialTable, { Column } from 'material-table';
+import { Context, ContextProps } from './Store'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-  
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
+
 interface Row {
   name: string;
   surname: string;
@@ -14,85 +31,68 @@ interface TableState {
   columns: Array<Column<Row>>;
   data: Row[];
 }
-  export default function Table() {
 
-    const { state, dispatch } = useContext<ContextProps>(Context);
+export default function Table() {
+  const classes = useStyles();
+  const { state, dispatch } = useContext<ContextProps>(Context);
+  const [table, setTable] = useState(Object.keys(state)[0]);
 
-    console.log(state)
 
-    // const [state, setState] = React.useState<TableState>({
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setTable(event.target.value as string);
+  };
 
-    //   columns: [
-    //     { title: 'Name', field: 'name' },
-    //     { title: 'Surname', field: 'surname' },
-    //     { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-    //     {
-    //       title: 'Birth Place',
-    //       field: 'birthCity',
-    //       lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-    //     },
-    //   ],
-    //   data: [
-    //     { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-    //     {
-    //       name: 'Zerya Betül',
-    //       surname: 'Baran',
-    //       birthYear: 2017,
-    //       birthCity: 34,
-    //     },
-    //   ],
-    // });
+  const tableKeys = Object.keys(state[table].values)
 
-    return (
-      <div>
-      {/* {state} */}
-      </div>
 
-    )
-  }
-  
-    return (
-      <MaterialTable
-        title="Editable Example"
-        columns={state.columns}
-        data={state.data}
-        editable={{
-          onRowAdd: newData =>
-            new Promise(resolve => {
-              setTimeout(() => {
-                resolve();
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data.push(newData);
-                  return { ...prevState, data };
-                });
-              }, 600);
-            }),
-          onRowUpdate: (newData, oldData) =>
-            new Promise(resolve => {
-              setTimeout(() => {
-                resolve();
-                if (oldData) {
-                  setState(prevState => {
-                    const data = [...prevState.data];
-                    data[data.indexOf(oldData)] = newData;
-                    return { ...prevState, data };
-                  });
-                }
-              }, 600);
-            }),
-          onRowDelete: oldData =>
-            new Promise(resolve => {
-              setTimeout(() => {
-                resolve();
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data.splice(data.indexOf(oldData), 1);
-                  return { ...prevState, data };
-                });
-              }, 600);
-            }),
-        }}
-      />
-    );
-  }
+
+   const keyValue = tableKeys.map( k => state[table].values[k].map(e => ({[k]: e}) ))
+
+
+   const getTableData = ():any => {
+     let res:any = []
+     keyValue.forEach(arr =>{
+       arr.forEach( (el, idx) => {
+         res[idx] = {...res[idx], ...el}
+       })
+     } )
+     return res
+   }
+
+   let tableData = getTableData()
+
+   useEffect(()=> {
+     tableData = getTableData();
+
+   },[table])
+
+   console.log(keyValue)
+   console.log(tableData)
+   
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="dictionary">Dictionaries</InputLabel>
+        <Select
+          labelId="Dictionary"
+          id="dic-select"
+          value={table}
+          onChange={handleChange}
+        >
+          {Object.keys(state).map(name =>
+            <MenuItem key={name} value={name}>{name}</MenuItem>
+          )}
+
+        </Select>
+      </FormControl>
+
+    <MaterialTable
+          title={table}
+          columns={ tableKeys.map( k => ({title: k, field: k}))  }
+          data={tableData}
+        />
+    </div>
+  );
+}
+
